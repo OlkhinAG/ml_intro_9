32ddf2ead8a4c450b29b19e27dc8817459395fc5
Andrey <Andrey@DESKTOP-OMHSODK>
2022-05-10 21:30:49 +0400

    replace TSNE with ICA

--- src/ml_intro_9/pipeline.py
+++ src/ml_intro_9/pipeline.py
@@ -2,21 +2,20 @@ from sklearn.pipeline import Pipeline
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, FastICA
 
 def create_pipeline(
-    random_state: int, use_scaler: bool, model: str, use_TSNE: bool, use_PCA: bool, model_params
+    random_state: int, use_scaler: bool, model: str, use_ICA: bool, use_PCA: bool, model_params
 ) -> Pipeline:
     pipeline_steps = []
     if use_scaler:
         pipeline_steps.append(("scaler", StandardScaler()))
 
     if use_PCA:
-        pipeline_steps.append(("PCA", PCA(n_components=0.9, svd_solver='full', random_state=random_state)))
+        pipeline_steps.append(("PCA", PCA(n_components=0.95, svd_solver='full', random_state=random_state)))
 
-    if use_TSNE:
-        pipeline_steps.append(("TSNE", TSNE(n_components=2, random_state=random_state)))
+    if use_ICA:
+        pipeline_steps.append(("ICA", FastICA(n_components=5, random_state=random_state)))
 
     if model == 'KNeighbors':
         pipeline_steps.append(
