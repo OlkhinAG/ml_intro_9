6b7564fdd1e08b7ea05dbb17d6441c088cf35a23
Andrey <Andrey@DESKTOP-OMHSODK>
2022-05-10 21:13:29 +0400

    feature eng first try

--- src/ml_intro_9/pipeline.py
+++ src/ml_intro_9/pipeline.py
@@ -2,14 +2,21 @@ from sklearn.pipeline import Pipeline
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
 
 def create_pipeline(
-    use_scaler: bool, model: str, model_params
+    random_state: int, use_scaler: bool, model: str, use_TSNE: bool, use_PCA: bool, model_params
 ) -> Pipeline:
     pipeline_steps = []
     if use_scaler:
         pipeline_steps.append(("scaler", StandardScaler()))
+
+    if use_PCA:
+        pipeline_steps.append(("PCA", PCA(n_components=0.9, svd_solver='full', random_state=random_state)))
+
+    if use_TSNE:
+        pipeline_steps.append(("TSNE", TSNE(n_components=2, random_state=random_state)))
 
     if model == 'KNeighbors':
         pipeline_steps.append(
@@ -26,7 +33,7 @@ def create_pipeline(
             (
                 "classifier",
                 RandomForestClassifier(
-                    **model_params
+                    random_state=random_state, **model_params
                 ),
             )
         )
