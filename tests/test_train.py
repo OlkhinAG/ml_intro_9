ea7622b90384a862a127fd1e7e6a318434a1531b
Andrey <Andrey@DESKTOP-OMHSODK>
2022-05-11 19:50:50 +0400

    Update test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -12,7 +12,7 @@ def runner() -> CliRunner:
 def test_error_for_invalid_model(
     runner: CliRunner
 ) -> None:
-    """It fails when test split ratio is greater than 1."""
+    """It fails given invalid model name."""
     result = runner.invoke(
         train,
         [
@@ -22,4 +22,26 @@ def test_error_for_invalid_model(
     )
     assert result.exit_code == 2
     assert "Invalid value for '--model'" in result.output
+
+def test_for_correct_model(
+    runner: CliRunner
+) -> None:
+    """It fails when model not produced or invalid model produced."""
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+
+        result = runner.invoke(
+            train,
+            [
+                "-d",
+                '../tests/sample_train.csv',
+                "-s",
+                "test_model.joblib"
+            ],
+        )
+    assert result.exit_code == 0
+    assert "accuracy 0.99" in result.output
+    assert "model saved in " in result.output
+
+
 
