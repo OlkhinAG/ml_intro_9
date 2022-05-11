4e945942a78b7cd90c3f6a7b22cb7bb8d2496e1e
Andrey <Andrey@DESKTOP-OMHSODK>
2022-05-11 19:32:46 +0400

    Create test_train.py

---
+++ tests/test_train.py
@@ -0,0 +1,24 @@
+from click.testing import CliRunner
+import pytest
+
+from ml_intro_9.train import train
+
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture providing click runner."""
+    return CliRunner()
+
+
+def test_error_for_invalid_test_split_ratio(
+    runner: CliRunner
+) -> None:
+    """It fails when test split ratio is greater than 1."""
+    result = runner.invoke(
+        train,
+        [
+            "--model",
+            'RTYRT',
+        ],
+    )
+    assert result.exit_code == 2
+    assert "Invalid value for '--model'" in result.output
